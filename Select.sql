7e3f4572ff78b8dc4f8dfdc0e1e1544a36ac68e2
Andrew Zhelnovach <hrthefreeboy@gmail.com>
2023-06-26 11:53:30 +0300

    Select.sql modified

--- Select.sql
+++ Select.sql
@@ -1,4 +1,4 @@
-# Задание 2
+-- Задание 2
 
 select name, duration from song
 where duration = (select MAX(duration) from song);
@@ -13,26 +13,38 @@ select name from performer
 where name not like '% %';
 
 select name from song
-where name like '%my%' or name like '%My%' or name like '%мой%' or name like '%Мой%';
+where name ilike 'my %'
+or name ilike '% my'
+or name ilike '% my %'
+or name ilike 'my'
+or name ilike 'мой %'
+or name ilike '% мой'
+or name ilike '% мой %'
+or name ilike 'мой';
 
-# Задание 3
+-- Задание 3
 
 select name, count(*)  from genre
 join genre_performer on genre.genre_id = genre_performer.genre_id
-group by name
+group by name;
 
 select count(song.name) from song
 join album on song.album_id = album.album_id
-where album.year between 2019 and 2020
+where album.year between 2019 and 2020;
 
 select album.name, AVG(song.duration) from song
 join album on song.album_id = album.album_id
-group by album.name
+group by album.name;
 
-select performer.name from performer
-join performer_album on performer.performer_id = performer_album.performer_id
-join album on performer_album.album_id = album.album_id
-where year <> 2020;
+select name
+from performer
+where name not in (
+    select performer.name
+    from performer
+    join performer_album on performer.performer_id = performer_album.performer_id
+    join album on performer_album.album_id = album.album_id
+    where album.year = 2020
+   );
 
 select distinct collection.name from performer
 join performer_album on performer.performer_id = performer_album.performer_id
@@ -40,4 +52,4 @@ join album on performer_album.album_id = album.album_id
 join song on album.album_id = song.album_id
 join song_collection on song.song_id = song_collection.song_id
 join collection on song_collection.collection_id = collection.collection_id
-where performer.name = 'HOYO-MIX'
+where performer.name = 'HOYO-MIX';
